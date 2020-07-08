7cb50e962aa0d6980b320b0da3cc7644a91ed724
luo8866 <1051937356@qq.com>
2020-07-08 10:34:56 +0800

    one

---
+++ one.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+type(3)
+
+
+# In[3]:
+
+
+type(3.0)
+
+
+# In[4]:
+
+
+x = 6
+x
+
+
+# In[5]:
+
+
+2+5
+
+
+# In[6]:
+
+
+123455667654321134*6554322234556778
+
+
+# In[7]:
+
+
+2/5
+
+
+# In[8]:
+
+
+2.0/5
+
+
+# In[9]:
+
+
+2.0/5.0
+
+
+# In[10]:
+
+
+2/5
+
+
+# In[11]:
+
+
+5/2
+
+
+# In[12]:
+
+
+7/2
+
+
+# In[13]:
+
+
+0.1+0.2
+
+
+# In[14]:
+
+
+from_future_import division
+5/2
+
+
+# In[ ]:
+
+
+from _future _import division
+5/2
+
+
+# In[ ]:
+
+
+round(1.2345,3)
+
+
+# In[ ]:
+
+
+import math
+
+
+# In[ ]:
+
+
+math.pi
+
+
+# In[ ]:
+
+
+dir(math)
+
+
+# In[ ]:
+
+
+math.pow(4,2)
+
+
+# In[ ]:
+
+
+4*2
+
+
+# In[ ]:
+
+
+help(round)
+
+
+# In[ ]:
+
+
+print"hello world"
+
+
+# In[ ]:
+
+
+print("hello world")
+
+
+# In[ ]:
+
+
+
+
